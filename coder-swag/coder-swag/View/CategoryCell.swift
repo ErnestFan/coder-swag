b7bb738d0010342fe210f8e53f1c975470a212dd
ErnestFan <ernestfan1019@gmail.com>
2018-01-11 16:30:24 -0800

    navigation controller and table view

---
+++ coder-swag/coder-swag/View/CategoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Ernest Fan on 2018-01-11.
+//  Copyright © 2018 ERF. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
