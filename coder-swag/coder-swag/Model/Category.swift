100c30c58aa9d84a5cdfbb82111d9ea061c72e34
ErnestFan <ernestfan1019@gmail.com>
2018-01-12 21:28:01 -0800

    DataService, VC conform to tableview func

---
+++ coder-swag/coder-swag/Model/Category.swift
@@ -0,0 +1,9 @@
+//
+//  Category.swift
+//  coder-swag
+//
+//  Created by Ernest Fan on 2018-01-11.
+//  Copyright © 2018 ERF. All rights reserved.
+//
+
+import Foundation
---
+++ coder-swag/coder-swag/Services/DataService.swift
@@ -0,0 +1,9 @@
+//
+//  DataService.swift
+//  coder-swag
+//
+//  Created by Ernest Fan on 2018-01-11.
+//  Copyright © 2018 ERF. All rights reserved.
+//
+
+import Foundation
