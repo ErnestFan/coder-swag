cb94013252877345b4dea0d97b4f4a03c5ef45de
ErnestFan <ernestfan1019@gmail.com>
2018-01-13 16:29:17 -0800

    Collection View, Product Model, DataService

---
+++ coder-swag/coder-swag/Model/Product.swift
@@ -0,0 +1,9 @@
+//
+//  Product.swift
+//  coder-swag
+//
+//  Created by Ernest Fan on 2018-01-13.
+//  Copyright © 2018 ERF. All rights reserved.
+//
+
+import Foundation
---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Ernest Fan on 2018-01-13.
+//  Copyright © 2018 ERF. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+}
